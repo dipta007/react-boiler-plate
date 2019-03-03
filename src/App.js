30583fcb78ff6ee8edc391878a95ff04f6e027ec
Shubhashis Roy <iamdipta@gmail.com>
2019-03-04 02:54:59 +0600

    added husky & lint-staged

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import test from './Component/test';
 
 class App extends Component {
-  render() {
-    return (
-      <>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/emailsender" exact component={} />
-          </Switch>
-        </BrowserRouter>
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/emailsender" exact component={test} />
+                    </Switch>
+                </BrowserRouter>
+            </>
+        );
+    }
 }
 
 export default App;
---
+++ src/Component/test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class test extends Component {
+    render() {
+        const { test1 } = this.state;
+        return <div>{test1}</div>;
+    }
+}
+
+test.propTypes = {};
+
+export default test;
+
+it('skip', () => {});
